331925265cd15a47cbc6421880bc6f4b7ade989b
Ra.khaihan <rakharaihan2244@gmail.com>
2025-03-08 21:47:05 +0700

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022300046
+{
+    internal class KodePos
+    {
+        public enum Kelurahan
+        {
+            Batununggal, Kujangsari, Mengger, Wales, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja
+        }
+
+        public static int getKodePos(Kelurahan kelurahan)
+        {
+            int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+            return kodePos[(int)kelurahan];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul4_103022300046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan nama kelurahan: ");
+            string inputKelurahan = Console.ReadLine();
+
+            KodePos.Kelurahan kelurahan;
+            bool isValidKelurahan = Enum.TryParse(inputKelurahan,true ,out kelurahan);
+
+            if (isValidKelurahan)
+            {
+                int kodePos = KodePos.getKodePos(kelurahan);
+                Console.WriteLine($"Kode Pos untuk {inputKelurahan}: {kodePos}");
+            }
+            else
+            {
+                Console.WriteLine("Kelurahan tidak valid.");
+            }
+        }
+    }
+}
